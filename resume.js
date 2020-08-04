0cfc4b283e0cd26c16f666ad41562d056795628c
Pereira <rbipp010@BKUSMAC-1NT8P3XY.local>
2020-08-04 14:13:52 -0400

    updates

--- resume.js
+++ resume.js
@@ -14,7 +14,8 @@ function renderWorkExperience(workExperience) {
     </ul>
   </div>`;
 } 
-  const renderEducationalExperience = educationalExperience => `
+
+const renderEducationalExperience = educationalExperience => `
   <div>
     <span class="date">${educationalExperience.dates}</span>
     <h3>
@@ -30,7 +31,8 @@ function renderWorkExperience(workExperience) {
         .join('')}
     </ul>
   </div>`;
-  function renderSkills(skill) {
+
+function renderSkills(skill) {
     return `
     <div>
       <span class="date">${skill.dates}</span>
@@ -43,133 +45,86 @@ function renderWorkExperience(workExperience) {
       ${skill.details.map(detail => `<li>${detail}</li>`).join('')}
       </ul>
     </div>`;
-  }
+}
 
-  const fetchData = async () => {
-    const response = await fetch('./data.json');
-    const data = await response.json();
+const fetchData = async () => {
+  const response = await fetch(
+    'https://andyweiss1982.github.io/resume_template/data.json'
+  );
+  const data = await response.json();
   
-    document.title = data.name;
+  document.title = data.name;
 
-    const name = document.querySelector('#name');
-    name.textContent = data.name;
+  const name = document.querySelector('#name');
+  name.textContent = data.name;
 
-    const emojiContainer = document.querySelector('h1 span[role="img"]');
-    emojiContainer.textContent = data.mainEmoji.character;
-    emojiContainer.setAttribute('aria-label', data.mainEmoji.ariaLabel);
+  const emojiContainer = document.querySelector('h1 span[role="img"]');
+  emojiContainer.textContent = data.mainEmoji.character;
+  emojiContainer.setAttribute('aria-label', data.mainEmoji.ariaLabel);
 
-    const phoneNumberContainer = document.querySelector('#phone-number');
-    phoneNumberContainer.textContent = data.phoneNumber;
-    const phoneLink = document.querySelector('li:nth-child(1) a');
-    phoneLink.href += data.phoneNumber;
+  const phoneNumberContainer = document.querySelector('#phone-number');
+  phoneNumberContainer.textContent = data.phoneNumber;
+  const phoneLink = document.querySelector('li:nth-child(1) a');
+  phoneLink.href += data.phoneNumber;
 
-    const emailContainer = document.querySelector('#email');
-    emailContainer.textContent = data.email;
-    const emailLink = document.querySelector('li:nth-child(2) a');
-    emailLink.href += data.email;
+  const emailContainer = document.querySelector('#email');
+  emailContainer.textContent = data.email;
+  const emailLink = document.querySelector('li:nth-child(2) a');
+  emailLink.href += data.email;
 
-    const githubContainer = document.querySelector('#github-link');
-    githubContainer.textContent += data.githubHandle;
-    const githubLink = document.querySelector('li:nth-child(3) a');
-    githubLink.href += data.githubHandle;
+  const githubContainer = document.querySelector('#github-link');
+  githubContainer.textContent += data.githubHandle;
+  const githubLink = document.querySelector('li:nth-child(3) a');
+  githubLink.href += data.githubHandle;
 
-    const linkedInContainer = document.querySelector('#linkedin-link');
-    linkedInContainer.textContent += data.linkedInHandle;
-    const linkedInLink = document.querySelector('li:nth-child(4) a');
-    linkedInLink.href += data.linkedInHandle;
+  const linkedInContainer = document.querySelector('#linkedin-link');
+  linkedInContainer.textContent += data.linkedInHandle;
+  const linkedInLink = document.querySelector('li:nth-child(4) a');
+  linkedInLink.href += data.linkedInHandle;
 
-    const workExperienceHeadingEmojiContainer = document.querySelector(
+  const workExperienceHeadingEmojiContainer = document.querySelector(
       "h2:nth-of-type(1) span[role='img']"
-    );
-    workExperienceHeadingEmojiContainer.textContent =
+  );
+  workExperienceHeadingEmojiContainer.textContent =
       data.headingEmojis.workExperience.character;
-    workExperienceHeadingEmojiContainer.setAttribute(
+  workExperienceHeadingEmojiContainer.setAttribute(
       'aria-label',
       data.headingEmojis.workExperience.ariaLabel
-    );
+  );
     const educationalExperienceHeadingEmojiContainer = document.querySelector(
       "h2:nth-of-type(2) span[role='img']"
     );
-    educationalExperienceHeadingEmojiContainer.textContent =
+  educationalExperienceHeadingEmojiContainer.textContent =
       data.headingEmojis.educationExperience.character;
-    educationalExperienceHeadingEmojiContainer.setAttribute(
+  educationalExperienceHeadingEmojiContainer.setAttribute(
       'aria-label',
       data.headingEmojis.educationExperience.ariaLabel
-    );
-    const skillsHeadingEmojiContainer = document.querySelector(
+  );
+  const skillsHeadingEmojiContainer = document.querySelector(
       "h2:nth-of-type(3) span[role='img']"
-    );
-    skillsHeadingEmojiContainer.textContent = data.headingEmojis.skills.character;
-    skillsHeadingEmojiContainer.setAttribute(
+  );
+  skillsHeadingEmojiContainer.textContent = data.headingEmojis.skills.character;
+  skillsHeadingEmojiContainer.setAttribute(
       'aria-label',
       data.headingEmojis.skills.ariaLabel
-    );
+  );
+  
+  const workExperienceContainer = document.querySelector('#work-experience');
+  const eachWorkExperienceHTML = data.workExperiences.map(renderWorkExperience);
+  const allWorkExperiencesHTML = eachWorkExperienceHTML.join('');
+  workExperienceContainer.innerHTML = allWorkExperiencesHTML;
 
-    function renderWorkExperience(workExperience) {
-      return `
-      <div>
-        <span class="date">${workExperience.dates}</span>
-        <h3>
-          <span role="img" aria-label="${workExperience.ariaLabel}">${
-        workExperience.emoji
-      }</span>
-          ${workExperience.jobTitle}<span class="comma">,</span>
-          <span class="light">${workExperience.institution}</span>
-        </h3>
-        <ul>
-          ${workExperience.details.map(detail => `<li>${detail}</li>`).join('')}
-        </ul>
-      </div>`;
-    }
-
-    const workExperienceContainer = document.querySelector('#work-experience');
-    const eachWorkExperienceHTML = data.workExperiences.map(renderWorkExperience);
-    const allWorkExperiencesHTML = eachWorkExperienceHTML.join('');
-    workExperienceContainer.innerHTML = allWorkExperiencesHTML;
-
-    const renderEducationalExperience = educationalExperience => `
-      <div>
-        <span class="date">${educationalExperience.dates}</span>
-        <h3>
-          <span role="img" aria-label="${educationalExperience.ariaLabel}">${
-      educationalExperience.emoji
-    }</span>
-          ${educationalExperience.university}<span class="comma">,</span>
-          <span class="light">${educationalExperience.school}</span>
-        </h3>
-        <ul>
-          ${educationalExperience.details
-            .map(detail => `<li>${detail}</li>`)
-            .join('')}
-        </ul>
-      </div>`;
-
-    const educationContainer = document.querySelector('#education');
-    const eachEducationalExperienceHTML = data.educationalExperiences.map(
-      renderEducationalExperience
-    );
-    const allEducationalExperiencesHTML = eachEducationalExperienceHTML.join('');
-    educationContainer.innerHTML = allEducationalExperiencesHTML;
-
-    function renderSkills(skill) {
-      return `
-      <div>
-        <span class="date">${skill.dates}</span>
-        <h3>
-          <span role="img" aria-label="${skill.ariaLabel}">${skill.emoji}</span>
-          ${skill.skillone}<span class="comma">,</span>
-          <span class="light">${skill.skilltwo}</span>
-        </h3>
-        <ul>
-        ${skill.details.map(detail => `<li>${detail}</li>`).join('')}
-        </ul>
-      </div>`;
-    }
-
-    const skillContainer = document.querySelector(`#skills`);
-    const eachSkillHTML = data.skills.map(renderSkills);
-    const allSkillsHTML = eachSkillHTML.join('');
-    skillContainer.innerHTML = allSkillsHTML;
-  };
+  const educationContainer = document.querySelector('#education');
+  const eachEducationalExperienceHTML = data.educationalExperiences.map(
+    renderEducationalExperience
+  );
+  const allEducationalExperiencesHTML = eachEducationalExperienceHTML.join('');
+  educationContainer.innerHTML = allEducationalExperiencesHTML;
+  
+  const skillContainer = document.querySelector(`#skills`);
+  const eachSkillHTML = data.skills.map(renderSkills);
+  const allSkillsHTML = eachSkillHTML.join('');
+  skillContainer.innerHTML = allSkillsHTML;
+};
 
 fetchData();
